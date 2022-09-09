09e7f2c3336e195ba82955a5ed8382961b2b7118
Ludovic Spina <74927851+ludovicspina@users.noreply.github.com>
2022-09-09 14:29:19 +0200

    hotfix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,12 +8,12 @@
             <caption class="p-5 text-lg font-semibold text-left text-gray-900">
                 <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Liste des objets distribués durant les Clockworks et Meteonyker.</p>
                 @if(Auth::user())
-                    <form action="{{ route('cdgAdd') }}" method="POST">
+                    <form action="{{ route('cdgAdd') }}" class="mt-4" method="POST">
                         @csrf
                         <div class="grid md:grid-cols-3 md:gap-6">
                             <div class="relative z-0 mb-6 w-48 group">
                                 <input type="text" name="objet" id="floating_first_name"
-                                       class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                       class="block py-2.5 px-0 w-full text-sm text-gray-200 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                                        placeholder=" " required/>
                                 <label for="floating_first_name"
                                        class="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nom
@@ -21,7 +21,7 @@
                             </div>
                             <div class="relative z-0 mb-6 w-48 group">
                                 <input type="text" name="joueur" id="floating_last_name"
-                                       class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                       class="block py-2.5 px-0 w-full text-sm text-gray-200 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                                        placeholder=" " required/>
                                 <label for="floating_last_name"
                                        class="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Joueur</label>
@@ -65,8 +65,12 @@
 
 
     @if(session('status'))
-        <div class="alert alert-success">
-            {{ session('status') }}
+        <div class="flex p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800" role="alert">
+            <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+            <span class="sr-only">Info</span>
+            <div>
+                <span class="font-medium">{{ session('status') }}</span>
+            </div>
         </div>
     @endif
 
