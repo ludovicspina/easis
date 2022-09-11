4bdbf364b2bf328a62cdcc91429f2bef61a651c0
Ludovic Spina <74927851+ludovicspina@users.noreply.github.com>
2022-09-11 19:00:04 +0200

    button

--- resources/views/hdv.blade.php
+++ resources/views/hdv.blade.php
@@ -122,7 +122,7 @@
                         </div>
                     </div>
                     <button type="submit"
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-64 md:w-full sm:w-auto px-5 py-2.5 text-center">
                         Valider
                     </button>
                 </form>
