5b9cf0c296bae64b02e584bfa6da7ef0445aa343
Ludovic Spina <74927851+ludovicspina@users.noreply.github.com>
2022-09-11 20:45:16 +0200

    hotfix

--- resources/views/hdv.blade.php
+++ resources/views/hdv.blade.php
@@ -120,15 +120,6 @@
                             <label for="floating_first_name"
                                    class="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Quantité</label>
                         </div>
-                        @if(Auth::user())
-                            <div class="relative z-0 mb-6 w-48 group">
-                                <input type="number" name="quantiteRecherche" id="quantiteRecherche"
-                                       class="block py-2.5 px-0 w-full text-sm text-gray-200 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                       placeholder=" " required/>
-                                <label for="floating_first_name"
-                                       class="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Quantité</label>
-                            </div>
-                        @endif
                     </div>
                     <button type="submit"
                             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-64 md:w-full sm:w-auto px-5 py-2.5 text-center">
