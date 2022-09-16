ed07f40460c10ba219876f0774d56e5d200b4ae9
Ludovic Spina <74927851+ludovicspina@users.noreply.github.com>
2022-09-16 18:47:36 +0200

    htofix

--- resources/views/hdv.blade.php
+++ resources/views/hdv.blade.php
@@ -126,7 +126,7 @@
                                                        class="block py-2.5 px-0 w-full text-sm text-gray-200 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                                                        placeholder=" "/>
                                                 <label for="floating_first_name"
-                                                       class="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Niveau</label>
+                                                       class="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">+</label>
                                             </div>
                                             <div class="relative z-0 mb-6 w-16 group">
                                                 <input type="number" name="quantite" id="quantite"
