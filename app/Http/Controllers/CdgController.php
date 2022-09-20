9484713cb77102bac7d393a3ca5dc96c9c5d1a62
Ludovic Spina <74927851+ludovicspina@users.noreply.github.com>
2022-09-20 21:59:10 +0200

    cdgArchive middle

--- app/Http/Controllers/CdgController.php
+++ app/Http/Controllers/CdgController.php
@@ -10,7 +10,7 @@ class CdgController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('membre');
     }
 
     public function index()
